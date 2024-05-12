e817964d3ad11bd898a9491c64669698cf5f336d
NicolasBlaiotta <nicolasvilas1962@gmail.com>
2024-05-11 22:38:59 -0300

    Agregar archivos de proyecto.

---
+++ frmFirma.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace pryLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblFirmar = new Label();
+            BtnLimpiar = new Button();
+            pictureBox1 = new PictureBox();
+            btnGuardar = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lblFirmar
+            // 
+            lblFirmar.AutoSize = true;
+            lblFirmar.Location = new Point(78, 44);
+            lblFirmar.Margin = new Padding(4, 0, 4, 0);
+            lblFirmar.Name = "lblFirmar";
+            lblFirmar.Size = new Size(44, 15);
+            lblFirmar.TabIndex = 1;
+            lblFirmar.Text = "Firmar:";
+            // 
+            // BtnLimpiar
+            // 
+            BtnLimpiar.Location = new Point(534, 254);
+            BtnLimpiar.Margin = new Padding(4, 3, 4, 3);
+            BtnLimpiar.Name = "BtnLimpiar";
+            BtnLimpiar.Size = new Size(88, 27);
+            BtnLimpiar.TabIndex = 2;
+            BtnLimpiar.Text = "Limpiar";
+            BtnLimpiar.UseVisualStyleBackColor = true;
+            BtnLimpiar.Click += BtnLimpiar_Click_1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(78, 102);
+            pictureBox1.Margin = new Padding(4, 3, 4, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(408, 359);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            pictureBox1.MouseDown += pictureBox1_MouseDown_1;
+            pictureBox1.MouseMove += pictureBox1_MouseMove_1;
+            pictureBox1.MouseUp += pictureBox1_MouseUp_1;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(534, 204);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(88, 30);
+            btnGuardar.TabIndex = 3;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(933, 519);
+            Controls.Add(btnGuardar);
+            Controls.Add(BtnLimpiar);
+            Controls.Add(lblFirmar);
+            Controls.Add(pictureBox1);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label lblFirmar;
+        private Button BtnLimpiar;
+        private Button btnGuardar;
+    }
+}
+
---
+++ frmFirma.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryLab3
+{
+    public partial class Form1 : Form
+    {
+        private Point? previousPoint;
+        private Pen pen;
+        private Bitmap firmaBitmap = new Bitmap(382, 217);
+        public Form1()
+        {
+            InitializeComponent();
+            pen = new Pen(Color.Black, 2); // Color y grosor del trazo
+            pictureBox1.BackColor = Color.White; // Color de fondo del PictureBox
+
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            using (var g = e.Graphics)
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            }
+        }
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            previousPoint = e.Location;
+        }
+
+        private void pictureBox1_MouseUp_1(object sender, MouseEventArgs e)
+        {
+            previousPoint = null;
+        }
+
+        private void pictureBox1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            if (previousPoint != null && e.Button == MouseButtons.Left)
+            {
+                using (Graphics g = pictureBox1.CreateGraphics())
+                {
+                    g.DrawLine(pen, previousPoint.Value, e.Location);
+                }
+                previousPoint = e.Location;
+            }
+        }
+
+        private void BtnLimpiar_Click_1(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh(); // Limpia la firma
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show("¿Estás seguro de que quieres guardar la firma?", "Confirmación", MessageBoxButtons.OKCancel);
+
+            if (resultado == DialogResult.OK)
+            {
+                // Obtener la ruta de la carpeta "Firmas" en relación con la ubicación del ejecutable
+                string carpetaFirmas = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Firmas");
+
+                // Verificar si la carpeta "Firmas" existe, si no, crearla
+                if (!Directory.Exists(carpetaFirmas))
+                {
+                    Directory.CreateDirectory(carpetaFirmas);
+                }
+
+                // Obtener la fecha actual
+                DateTime fechaActual = DateTime.Now;
+
+                // Construir el nombre de archivo con la fecha y hora actual
+                string nombreArchivo = $"{fechaActual:yyyyMMdd_HHmm}.jpg";
+
+                // Combinar la ruta de la carpeta "Firmas" con el nombre de archivo
+                string rutaCompleta = Path.Combine(carpetaFirmas, nombreArchivo);
+
+                // Guardar la imagen en la ruta especificada
+                firmaBitmap.Save(rutaCompleta, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                // Limpiar la firma en pantalla
+                using (Graphics papel = Graphics.FromImage(firmaBitmap))
+                {
+                    papel.Clear(Color.White);
+                }
+                pictureBox1.Refresh();
+            }
+        }
+    }
+}
+
+
